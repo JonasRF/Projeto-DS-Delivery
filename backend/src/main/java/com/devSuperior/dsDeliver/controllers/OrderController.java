d01ac1d642ad16b024b5fa7dc90988977282f996
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-10-29 16:22:17 -0300

    POST/ orders

--- backend/src/main/java/com/devSuperior/dsDeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devSuperior/dsDeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devSuperior.dsDeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devSuperior.dsDeliver.DTO.OrderDTO;
 import com.devSuperior.dsDeliver.services.OrderService;
@@ -17,10 +21,17 @@ public class OrderController {
 
 	@Autowired
 	OrderService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/devSuperior/dsDeliver/services/OrderService.java
+++ backend/src/main/java/com/devSuperior/dsDeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devSuperior.dsDeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devSuperior.dsDeliver.DTO.OrderDTO;
+import com.devSuperior.dsDeliver.DTO.ProductDTO;
 import com.devSuperior.dsDeliver.entities.Order;
+import com.devSuperior.dsDeliver.entities.OrderStatus;
+import com.devSuperior.dsDeliver.entities.Product;
 import com.devSuperior.dsDeliver.repositories.OrderRepository;
+import com.devSuperior.dsDeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
-	List<Order> list = orderRepository.findOrderWithProducts();
-	return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		List<Order> list = orderRepository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
 
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getReferenceById(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 }
